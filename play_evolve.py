2772af817c5f95aeb6b0a9a92e99670b4a2e6c9a
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2023-04-27 18:23:47 -0400

    Clean

--- play_evolve.py
+++ play_evolve.py
@@ -1,11 +1,7 @@
-import time
-import pygame
-from constants import TICK_LENGTH
 from FlappyBirdModel import FlappyBirdModel
 from FlappyBirdView import FlappyBirdView
 from FlappyBirdController import FlappyBirdController
 from EvolvePlayer import EvolvePlayer
-from joblib import load
 
 model = FlappyBirdModel()
 view = FlappyBirdView()
